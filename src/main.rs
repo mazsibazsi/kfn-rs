a4a7b1ed11dc4559a23cc76f3f0db0b49c53b730
Balázs Máhli <mahli@posteo.net>
2022-07-26 22:47:33 +0200

    Insert copyright file and prepare for argument reading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 mod kfn_io;
 
+use std::env;
 use kfn_io::KfnFile;
 
 fn main() {
+    /* let mut file = String::from("None");
+    let args: Vec<String> = env::args().collect();
+    match args[1].as_str() {
+        "-f" => {
+            if args.len() > 2 && args[2].contains(".kfn") {
+                file = String::from(args[2].clone());
+            }
+        },
+        _ => ()
+    }
+    dbg!(args);
+    if file == "None" {
+        return;
+    }*/
     let mut kfn = KfnFile::new("shika.kfn");
     kfn.dump().unwrap();
     dbg!(kfn.kfn_data.syncs.len(), kfn.kfn_data.text.len());
