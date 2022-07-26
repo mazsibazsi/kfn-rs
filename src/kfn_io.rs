537a457190b6e3189524415889257256ac15c742
Balázs Máhli <mahli@posteo.net>
2022-07-26 13:26:07 +0200

    implemented extraction, working

--- src/kfn_io.rs
+++ src/kfn_io.rs
@@ -1,14 +1,20 @@
-mod helpers;
+pub mod helpers;
 
-use core::{num, panic};
+use core::{panic};
 use std::fs;
 use std::error::Error;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
 use helpers::dump_hex;
 use helpers::FileType;
 use helpers::Entry;
 
+use crate::kfn_io::helpers::Header;
+
 #[derive(Debug)]
 pub struct KfnFile {
+    header: Header,
     file: Vec<u8>,
     read_head: usize,
     entries: Vec<Entry>,
@@ -16,14 +22,16 @@ pub struct KfnFile {
 
 impl KfnFile {
     pub fn read(filename: &str) -> Self {
-        let mut entries = Vec::new();
+        let entries = Vec::new();
+        let header = Header::new();
         Self { 
             file: match fs::read(filename) {
                 Ok(file) => file,
                 Err(e) => panic!("File not found! {}", e),
             },
             read_head: 0,
-            entries
+            entries,
+            header,
          }
     }
 
@@ -32,7 +40,7 @@ impl KfnFile {
         let signature = String::from_utf8(self.read_bytes(4))?;
         // if file signature is not KFNB, end parsing
         if signature != "KFNB" {
-            return Ok(false);
+            panic!("Bad signature error");
         }
 
         // reading the header
@@ -47,7 +55,14 @@ impl KfnFile {
                 },
                 2 => {
                     let buf = self.read_bytes(len_or_value);
-                    println!("{}, type 2, length {}, hex: {}", signature, len_or_value, dump_hex(buf));
+                    dbg!(&signature);
+                    match signature.as_str() {
+                        "TITL" => self.header.title = String::from_utf8(buf.clone()).unwrap_or("Unknown".to_string()),
+                        "ARTS" => self.header.artist = String::from_utf8(buf.clone()).unwrap_or("Unknown".to_string()),
+                        "KFNZ" => self.header.karafunizer = String::from_utf8(buf.clone()).unwrap_or("Unknown".to_string()),
+                        _ => ()
+                    }
+                    println!("{}, type 2, length {:#?}, hex: {}, string: {:?}", signature, len_or_value, dump_hex(&buf), String::from_utf8(buf));
                 },
                 _ => {
 
@@ -58,66 +73,93 @@ impl KfnFile {
                 break;
             }
         }
-        println!("Header ends at offset {}", self.read_head);
+        println!("header end: {}", self.read_head);
 
         // reading the directory
         let num_files = self.read_dword();
-        println!("Number of files: {}", num_files);
+        println!("# of files: {}", num_files);
 
-        for i in 0..num_files {
+        for _ in 0..num_files {
+
             let filename_len = self.read_dword();
             let filename = match String::from_utf8(self.read_bytes(filename_len)) {
                 Ok(s) => s,
                 Err(_) => panic!("Invalid filename in KFN file.")
             };
-            let file_type = self.read_dword();
-            let file_len1 = self.read_dword();
-            let file_offset = self.read_dword();
-            let file_len2 = self.read_dword();
-            let file_flags = self.read_dword();
+            let file_type = FileType::from(self.read_dword());
+            let len1 = self.read_dword() as usize;
+            let offset = self.read_dword() as usize;
+            let len2 = self.read_dword() as usize;
+            let flags = self.read_dword() as usize;
 
-            println!("File {}, type: {}, len1: {}, len2: {}, offset: {}, flags: {}",
-                                                                                filename,
-                                                                                file_type,
-                                                                                file_len1,
-                                                                                file_len2,
-                                                                                file_offset,
-                                                                                file_flags);
+            self.entries.push(Entry {
+                filename, file_type, len1, offset, len2, flags
+            });
         }
+
+        // readjust offset
+        for i in 0..self.entries.len() {
+            self.entries[i].offset += self.read_head;
+        }
+
         println!("Directory ends at offset {}", self.read_head);
 
         Ok(true)
     }
 
+    pub fn extract_all(&mut self) {
+        for i in 0..self.entries.len() {
+            self.extract(self.entries[i].clone(), self.entries[i].clone().filename);
+        }
+    }
+
+    /// Extracting a file from the entry to a deisgnated output.
+    fn extract(&mut self, entry: Entry, output_filename: String) {
+        // move read head to the beginning of the file
+        let mut path_str = self.header.title.clone();
+        path_str.push('/');
+        path_str.push_str(output_filename.as_str());
+        let path = Path::new(&path_str);
+        let prefix = path.parent().unwrap();
+        fs::create_dir_all(prefix).unwrap();
+        // create output file
+        dbg!(&self.header.title);
+        let mut output = File::create(path).unwrap();
+        // init buffer
+        let buf: Vec<u8> = Vec::from(&self.file[entry.offset..entry.offset+entry.len1]);
+        output.write_all(&buf).unwrap();
+        
+    }
+
     /// Helper IO function for reading a byte
     fn read_byte(&mut self) -> u8 {
-        let result = self.file[self.read_head];
+        let result = self.file[self.read_head as usize];
         self.read_head += 1;
         (result & 0xFF).into()
     }
 
     /// Helper IO function for reading a word
-    fn read_word(&mut self) -> u8 {
-        let b1 = self.read_byte();
-        let b2 = self.read_byte();
+    fn _read_word(&mut self) -> u16 {
+        let b1 = self.read_byte() as u16;
+        let b2 = self.read_byte() as u16;
 
         b2 << 8 | b1
     }
 
     /// Helper IO function for reading a dword
-    fn read_dword(&mut self) -> u8 {
+    fn read_dword(&mut self) -> u32 {
         let b1 = self.read_byte() as u32;
         let b2 = self.read_byte() as u32;
         let b3 = self.read_byte() as u32;
         let b4 = self.read_byte() as u32;
 
-        (b4 << 24 | b3 << 16 | b2 << 8 | b1) as u8
+        b4 << 24 | b3 << 16 | b2 << 8 | b1
     }
 
     /// Helper IO function for reading a specified amount fo bytes
-    fn read_bytes(&mut self, length: u8) -> Vec<u8> {
+    fn read_bytes(&mut self, length: u32) -> Vec<u8> {
         let mut array: Vec<u8> = Vec::with_capacity(length as usize);
-        for n in 0..length {
+        for _ in 0..length {
             array.push(self.read_byte());
         }
         array
--- src/kfn_io/helpers.rs
+++ src/kfn_io/helpers.rs
@@ -1,33 +1,60 @@
 use std::fmt::Write;
 
-pub fn dump_hex(array: Vec<u8>) -> String {
+pub fn dump_hex(array: &Vec<u8>) -> String {
     let mut output = String::new();
     for i in 0..array.len() {
         if i > 0 {
             output.push(' ');
         }
         let mut tmp = String::new();
-        write!(&mut tmp, "{:X} ", array[i] & 0xFF);
+        write!(&mut tmp, "{:X} ", array[i] & 0xFF).unwrap();
         output.push_str(&tmp);
     }
     output
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub enum FileType {
-    TYPE_SONGTEXT,
-    TYPE_MUSIC,
-    TYPE_IMAGE,
-    TYPE_FONT,
-    TYPE_VIDEO,
+    Songtext,
+    Music,
+    Image,
+    Font,
+    Video,
+    INVALID,
+}
+
+impl From<u32> for FileType {
+    fn from(numeric: u32) -> Self {
+        match numeric {
+            1 => FileType::Songtext,
+            2 => FileType::Music,
+            3 => FileType::Image,
+            4 => FileType::Font,
+            5 => FileType::Video,
+            _ => FileType::INVALID,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Entry {
+    pub file_type: FileType,
+    pub filename: String,
+    pub len1: usize,
+    pub offset: usize,
+    pub len2: usize,
+    pub flags: usize,
 }
 
 #[derive(Debug)]
-pub struct Entry {
-    pub f_type: FileType,
-    pub filename: String,
-    pub len1: u8,
-    pub len2: u8,
-    pub offset: u8,
-    pub flags: u8,
+pub struct Header {
+    pub title: String,
+    pub artist: String,
+    pub karafunizer: String,
+}
+
+impl Header {
+    pub fn new() -> Self {
+        Self { title: "".to_string(), artist: "".to_string(), karafunizer: "".to_string() }
+    }
 }
